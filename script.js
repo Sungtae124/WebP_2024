132807f71a8cb5816b93b2bbbc09886327bfd4c3
cheoljinn <sikdaep0622@gmail.com>
2024-11-21 07:11:26 +0900

    feat: 세부 페이지 레이아웃 #2
    세부 재생 페이지의 기본 틀 - 메인 패널과 버튼(기능 없음)을 배치했습니다

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    // 대체 텍스트 및 이미지로 초기 설정
+    const songTitle = document.getElementById("song-title");
+    const artistName = document.getElementById("artist-name");
+
+    songTitle.textContent = "Web Project";  // 노래 제목
+    artistName.textContent = "Park SeoYeon"; // 아티스트 이름
+
+    // Spotify API를 연결하면 여기서 fetch 호출
+
+
+
+});
